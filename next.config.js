6579383d62cd15a6a10f2c7a3b3d72f5865c5e9b
Mark <mark@remarkablemark.org>
2022-11-19 20:17:19 -0500

    build(next): configure the project to use the Edge Runtime
    https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#configure-the-project-to-use-the-edge-runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#configure-the-project-to-use-the-edge-runtime
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
