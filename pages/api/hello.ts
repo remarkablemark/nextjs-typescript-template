e329aac16bfb79f0eaac444882ef856f532b1e0d
Mark <mark@remarkablemark.org>
2022-11-19 20:36:08 -0500

    fix(api): rewrite hello.ts as an Edge API Route
    https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#configure-the-project-to-use-the-edge-runtime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+import type { NextRequest } from 'next/server';
 
-type Data = {
+interface Data {
   name: string;
-};
+}
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' });
+export default function handler(request: NextRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
