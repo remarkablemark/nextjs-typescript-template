822997e718838f82f8b28b8be9dbab1a7e9b2f3f
Mark <mark@remarkablemark.org>
2022-11-24 18:21:35 -0500

    chore(next): lint directories cypress and src
    https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
+  eslint: {
+    dirs: ['cypress', 'src'],
+  },
   // https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#configure-the-project-to-use-the-edge-runtime
   experimental: {
     runtime: 'experimental-edge',
