c1da824dba1b3960ab138041a4be00de14920622
Mark <mark@remarkablemark.org>
2022-11-24 16:35:14 -0500

    test(cypress): set baseUrl in e2e config

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,7 +1,7 @@
 describe('Index', () => {
   it('renders the index page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
 
     // The new page should contain an h1 with "Welcome to Next.js!"
     cy.get('h1').contains('Welcome to Next.js!');
