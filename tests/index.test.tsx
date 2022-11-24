e72273352a574fd0e958edd1f8e58ae450d79ca6
Mark <mark@remarkablemark.org>
2022-11-24 17:09:33 -0500

    test(index): fix lint errors

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,12 +1,14 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../pages/index";
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
 
-describe("Home", () => {
-  it("renders a heading", () => {
+import { render, screen } from '@testing-library/react';
+
+import Home from '../pages/index';
+
+describe('Home', () => {
+  it('renders a heading', () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
+    const heading = screen.getByRole('heading', {
       name: /welcome to next\.js!/i,
     });
 
