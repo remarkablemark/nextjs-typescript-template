8faa647090427fcf7bc5ae4736b97b1fef1f1ab2
Mark <mark@remarkablemark.org>
2025-08-14 22:12:18 -0400

    test(jest): fix import error
    ```
    Error: Jest: Failed to parse the TypeScript config file /home/runner/work/nextjs-typescript-template/nextjs-typescript-template/jest.config.ts
      Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/home/runner/work/nextjs-typescript-template/nextjs-typescript-template/node_modules/next/jest' imported from /home/runner/work/nextjs-typescript-template/nextjs-typescript-template/jest.config.ts
    Did you mean to import "next/jest.js"?
    ```

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'jest';
-import nextJest from 'next/jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -7,7 +7,8 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
-const customJestConfig: Config = {
+const config: Config = {
+  coverageProvider: 'v8',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/config/jest.setup.ts'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
@@ -16,4 +17,4 @@ const customJestConfig: Config = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig);
+export default createJestConfig(config);
